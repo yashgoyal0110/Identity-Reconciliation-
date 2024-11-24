e9635a89edeaf074aacd4d0ad2f475822a408ec4
yash goyal <yg364550@gmail.com>
2024-11-24 15:49:25 +0530

    server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import { identifyContact } from './controllers/controller.js';
+import router from './routes/route.js';
 dotenv.config()
 const app = express();
 app.use(express.json());
+
 
 app.get('/', async(req, res)=>{
     return res.status(200).json({message: 'test get api'});
 })
 
-app.post('api/vi', identifyContact)
+app.use('/api/v1', router);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
